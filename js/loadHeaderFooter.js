89ebca2261fb119f7fad7db96a355c275f918055
zhonghao <1573947261@qq.com>
2017-08-25 01:10:13 +0800

    部分尾部布局

--- js/loadHeaderFooter.js
+++ js/loadHeaderFooter.js
@@ -1,16 +1,4 @@
-define(["jquery", "cookie"], function($){
-	$.ajax({
-		type : 'get',
-		url : "/html/include/header.html",
-		success : function(data){
-			var _username = $.cookie("loginUser");
-			if (_username) { // 存在登录成功的用户
-				$(data).filter(".login_reg")
-					   .html(`欢迎你：${_username}`).end()
-					   .appendTo(".header");
-			} else {
-				$(data).appendTo(".header");
-			}
-		}
-	});
+define(["jquery"], function($){
+	$(".header").load("html/include/header.html");
+	$(".footer").load("html/include/footer.html");
 });
