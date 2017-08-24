4641b5f1356192c38bcb93b64422d41ff2f91516
zhonghao <1573947261@qq.com>
2017-08-25 03:49:41 +0800

    完成尾部布局及页面头尾加载

--- js/loadHeaderFooter.js
+++ js/loadHeaderFooter.js
@@ -1,4 +1,23 @@
-define(["jquery"], function($){
-	$(".header").load("html/include/header.html");
-	$(".footer").load("html/include/footer.html");
+define(["jquery", "cookie"], function($) {
+    $(".header").load("html/include/header.html");
+    $(".footer").load("html/include/footer.html");
+    $.get({
+        url: "html/include/header.html",
+        success: function(data) {
+            console.log(data);
+            if ($.cookie("loginUser")) {
+
+                console.log($.cookie("loginUser"));
+                var html = 
+                ` <ul class="top_right">
+        			<li style="border:0"><a href="#" style="border:0">${$.cookie("loginUser")}，你好</a></li>
+        			<li style="border:0"><a href="#" style="border:0">退出</a></li>
+        			<li><a href="#" style="border-left:0">我的订单</a></li>
+        			<li><a href="#">我的收藏</a></li>
+      			  </ul>
+				`;
+                $(".top_right").html(html);
+            }
+        }
+    });
 });
